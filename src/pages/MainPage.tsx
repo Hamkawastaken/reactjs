5e561cf242c2fba189ea9ac82ff155dd290b215b
Muhammad Hamka <hamkarifai49@gmail.com>
2025-05-04 22:16:40 +0700

    sixth commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const MainPage = () => {
+  const [fullNameInput, setFullNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+
+  const handleSubmit = () => {};
+
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-12 border py-4">
@@ -16,6 +23,35 @@ const MainPage = () => {
           quas harum laudantium perferendis quos voluptate! Obcaecati
           necessitatibus consequatur enim a ut tenetur
         </p>
+
+        <br />
+        <p> Full Name : {fullNameInput} </p>
+        <p> Email : {emailInput} </p>
+        <form action="" className="flex flex-col mt-4 border p-4 rounded-lg">
+          <label htmlFor="full-name">Full Name</label>
+          <input
+            onChange={(e) => setFullNameInput(e.target.value)}
+            type="text"
+            id="full-name"
+            className="border rounded-lg p-1"
+            value={fullNameInput}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={(e) => setEmailInput(e.target.value)}
+            type="text"
+            id="email"
+            className="border rounded-lg p-1"
+            value={emailInput}
+          />
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="rounded-lg font-semibold p-1 mt-2 bg-sky-500 text-white"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </>
   );
