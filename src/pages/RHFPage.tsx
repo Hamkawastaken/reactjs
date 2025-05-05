ad9042885e53b05a1ce0260e5102125150f39159
Muhammad Hamka <hamkarifai49@gmail.com>
2025-05-05 14:06:08 +0700

    nineth commit

--- src/pages/RHFPage.tsx
+++ src/pages/RHFPage.tsx
@@ -1,14 +1,25 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form"
+import { z } from "zod";
 
-type RegisterFormSchema = {
-    username: string;
-    password: string;
-}
+// type RegisterFormSchema = {
+//     username: string;
+//     password: string;
+// }
+
+const registerFormSchema = z.object({
+    username: z.string().min(3).max(10),
+    password: z.string().min(8),
+    age: z.coerce.number().min(18)
+})
+
+type RegisterFormSchema = z.infer<typeof registerFormSchema>
 
 const RHFPage = () => {
 
-
-    const form = useForm<RegisterFormSchema>();
+    const form = useForm<RegisterFormSchema>({
+        resolver: zodResolver(registerFormSchema)
+    });
 
     const handleRegisterUser = (values: RegisterFormSchema) => {
         alert("Form Submitted");
@@ -24,10 +35,23 @@ const RHFPage = () => {
                     Username : 
                     <input type="text" placeholder="Your Username" className="border rounded py-1 px-2" {...form.register("username")} />
                 </label>
+                <span className="text-red-500">
+                    {form.formState.errors.username?.message}
+                </span>
                 <label>
                     Password : 
                     <input type="password" placeholder="Your Password" className="border rounded py-1 px-2" {...form.register("password")} />
                 </label>
+                <span className="text-red-500">
+                    {form.formState.errors.password?.message}
+                </span>
+                <label>
+                    Age : 
+                    <input type="number" placeholder="Your Age" className="border rounded py-1 px-2" {...form.register("age")} />
+                </label>
+                <span className="text-red-500">
+                    {form.formState.errors.age?.message}
+                </span>
                 <button className="text-white bg-sky-500 font-semibold">Register</button>
             </form>
         </div>
